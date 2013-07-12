14e24b16f04baf58230b22a5765f1bb4f1d0f220
pinbelly <pinbelly@naver.com>
2013-07-12 17:45:19 +0900

    Makefile

--- hello.c
+++ hello.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 
-int main(int argc, char* argv[])
+int __init init_hello(void)
 {
-	int a, b;
-	int c = 0;
-	int i;
-
-	for (i=0; i<10; i++)
-	{
-		printf("a + b is %d\n", a+b);
-		a = a + 3;
-		b = b + 5;
-	}	
-
-	printf("(a + b) / c is %d\n", (a+b)/c);
+	printk(KERN_ALERT "[Module Message] Hello, Module.\n");
 	return 0;
 }
+
+void __exit exit_hello(void)
+{
+	printk(KERN_ALERT "[Module Message] Do you really want to break up with me? \n");
+}
+
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_LICENSE("GPL");
